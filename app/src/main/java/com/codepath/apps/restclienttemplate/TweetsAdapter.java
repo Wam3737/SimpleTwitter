59c9ca97ee8e6bbf32597db03f22be7059818ceb
Will <will.matthews1@aol.com>
2021-03-02 21:04:06 -0500

    Basic user stories complete

---
+++ app/src/main/java/com/codepath/apps/restclienttemplate/TweetsAdapter.java
@@ -0,0 +1,81 @@
+package com.codepath.apps.restclienttemplate;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.codepath.apps.restclienttemplate.models.Tweet;
+
+import java.util.List;
+
+
+public class TweetsAdapter extends RecyclerView.Adapter<TweetsAdapter.ViewHolder>{
+    Context context;
+    List<Tweet> tweets;
+    //pass in context and list of tweets
+    public TweetsAdapter(Context context, List<Tweet> tweets){
+        this.context = context;
+        this.tweets = tweets;
+    }
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(context).inflate(R.layout.item_tweet,  parent, false);
+
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+       Tweet tweet= tweets.get(position);
+       holder.bind(tweet);
+    }
+
+    @Override
+    // Clean all elements of the recycler
+
+    public int getItemCount() {
+        return tweets.size();
+    }
+
+    public void clear(){
+        tweets.clear();
+        notifyDataSetChanged();
+    }
+
+    public void addAll(List<Tweet> tweetList){
+        tweets.addAll(tweetList);
+        notifyDataSetChanged();
+    }
+
+    //for each row, inflate layout
+
+    //bind value based on position of element
+
+    //define a viewholder
+    public class ViewHolder extends RecyclerView.ViewHolder{
+        ImageView profileImage;
+        TextView body;
+        TextView handle;
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            profileImage = itemView.findViewById(R.id.ivProfile);
+            body = itemView.findViewById(R.id.tvTweet);
+            handle = itemView.findViewById(R.id.tvHandle);
+
+        }
+
+        public void bind(Tweet tweet) {
+            body.setText(tweet.body);
+            handle.setText(tweet.user.handle);
+            Glide.with(context).load(tweet.user.imageUrl).into(profileImage);
+        }
+    }
+}
---
+++ app/src/main/java/com/codepath/apps/restclienttemplate/models/User.java
@@ -0,0 +1,17 @@
+package com.codepath.apps.restclienttemplate.models;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class User {
+    public String handle;
+    public String name;
+    public String imageUrl;
+    public static User fromJson(JSONObject jsonObject) throws JSONException {
+        User user = new User();
+        user.handle = jsonObject.getString("screen_name");
+        user.name = jsonObject.getString("name");
+        user.imageUrl = jsonObject.getString("profile_image_url_https");
+        return user;
+    }
+}
